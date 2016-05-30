df449163048ce886b6c572dfd5172dfc814a07c9
jimmyPM <pardomendozajimmy@gmail.com>
2016-05-30 11:23:35 -0400

    Update slim.php

--- slim.php
+++ slim.php
@@ -1,14 +1,19 @@
 <?php
 require_once 'verdor/autolod.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
 $app=new \Slim\Slim();
 $app->get ("/hola/;nombre" ,  function ($nombre){
 echo 'Hola',$nombre;   
 });
 
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
 
-$app->post('/books', function () {
-    //Create book
+    return $response;
 });
 
 $app->run();
